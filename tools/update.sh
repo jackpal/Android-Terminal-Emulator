74a482afaa0af63556a711242328ca834e8cdc9d
Jack Palevich <jackpal@google.com>
2012-05-12 04:08:29 -0700

    Recursively look for android project files.

--- tools/update.sh
+++ tools/update.sh
@@ -1,1 +1,17 @@
-~/code/android-sdk-mac_x86/tools/android update project -p ~/code/Android-Terminal-Emulator --name "Term" --target "android-11"
+#!/bin/bash
+# You have to run this once in order for ant builds to work
+
+# assumes the Android SDK tool "android" is on the path
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ATE_ROOT="$( cd $DIR/.. && pwd )"
+
+echo "Updating android project files"
+
+PROJECT_FILES="$( find "$ATE_ROOT" -name project.properties )"
+
+for PROJECT_FILE in $PROJECT_FILES
+do
+    PROJECT_DIR="$( dirname "$PROJECT_FILE" )"
+    android update project -p "$PROJECT_DIR"
+done
