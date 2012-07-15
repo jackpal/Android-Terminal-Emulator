64d16cf32c78ce346313037ca95e4c3a164bd85c
Jack Palevich <jack@tulip.local>
2012-07-14 20:17:45 -0700

    Fix documentation for setting up inital build.
    Give a better error if the "android" tool is not on the path.

--- tools/update.sh
+++ tools/update.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # You have to run this once in order for ant builds to work
 
-# assumes the Android SDK tool "android" is on the path
+# Check that the Android SDK tool "android" is on the path
+
+ANDROID=android
+command -v "$ANDROID" >/dev/null 2>&1 || { echo >&2 "The $ANDROID tool is not on the current PATH.  Aborting."; exit 1; }
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ATE_ROOT="$( cd $DIR/.. && pwd )"
@@ -13,5 +16,5 @@ PROJECT_FILES="$( find "$ATE_ROOT" -name project.properties )"
 for PROJECT_FILE in $PROJECT_FILES
 do
     PROJECT_DIR="$( dirname "$PROJECT_FILE" )"
-    android update project -p "$PROJECT_DIR"
+    $ANDROID update project -p "$PROJECT_DIR"
 done
