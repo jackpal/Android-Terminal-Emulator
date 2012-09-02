7d01a8ce3fe4c4f1974abe9c231061e2a818225e
Jack Palevich <jackpal@google.com>
2012-09-02 15:59:47 -0700

    Use ANDROID_SDK_ROOT and ANDROID_NDK_ROOT environment variables

--- tools/update.sh
+++ tools/update.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 # You have to run this once in order for ant builds to work
+set -e
 
-# Check that the Android SDK tool "android" is on the path
+if [ -z "${ANDROID_SDK_ROOT+xxx}" ]; then
+	echo "Please define ANDROID_SDK_ROOT to point to the Android SDK"
+	exit 1
+fi
 
-ANDROID=android
-command -v "$ANDROID" >/dev/null 2>&1 || { echo >&2 "The $ANDROID tool is not on the current PATH.  Aborting."; exit 1; }
+if [ ! -d "$ANDROID_SDK_ROOT" ]; then
+    echo "The directory $ANDROID_SDK_ROOT = ${ANDROID_NDK_ROOT} does not exist."
+    exit 1
+fi
+
+ANDROID="$ANDROID_SDK_ROOT/tools/android"
+
+command -v "$ANDROID" >/dev/null 2>&1 || { echo >&2 "The $ANDROID tool is not found.  Aborting."; exit 1; }
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ATE_ROOT="$( cd $DIR/.. && pwd )"
