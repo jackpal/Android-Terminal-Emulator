b45102662afe705dbfa5e39f33a6937585037c52
Robert Ewald <robewald@gmx.net>
2012-09-06 18:20:17 +0200

    bits and pieces

--- libraries/emulatorview-test/src/jackpal/androidterm/emulatorview/TermKeyListenerTest.java
+++ libraries/emulatorview-test/src/jackpal/androidterm/emulatorview/TermKeyListenerTest.java
@@ -25,7 +25,7 @@ public class TermKeyListenerTest extends  AndroidTestCase {
 	
 	public void testKey_a() {
 		KeyEvent event = new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_A);
-		tkl_AltIsEsc.consumeKeyDownEvent(event);
+		tkl_AltIsEsc.keyDown(event);
 		byte[] res = tkl_AltIsEsc.getCharSequence();
 		assertNotNull(res);
 		assertEquals(0x61, res[0]);
@@ -33,7 +33,7 @@ public class TermKeyListenerTest extends  AndroidTestCase {
 	
 	public void testKey_X() {
 			KeyEvent event = new KeyEvent(1,2,KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_X, 0, KeyEvent.META_SHIFT_ON);
-			tkl_AltIsEsc.consumeKeyDownEvent(event);
+			tkl_AltIsEsc.keyDown(event);
 			byte[] res = tkl_AltIsEsc.getCharSequence();
 			assertNotNull(res);
 			assertEquals(0x58, res[0]);
@@ -41,7 +41,7 @@ public class TermKeyListenerTest extends  AndroidTestCase {
 
 	public void testKey_CTRL_c() {
 			KeyEvent event = new KeyEvent(1,2, KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_C, 0,  KeyEvent.META_CTRL_ON);
-			tkl_AltIsEsc.consumeKeyDownEvent(event);
+			tkl_AltIsEsc.keyDown(event);
 			byte[] res = tkl_AltIsEsc.getCharSequence();
 			assertNotNull(res);
 			assertEquals(0x03, res[0]);
@@ -49,7 +49,7 @@ public class TermKeyListenerTest extends  AndroidTestCase {
 
 	public void testKey_CTRL_c_no_esc() {
 		KeyEvent event = new KeyEvent(1,2, KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_C, 0,  KeyEvent.META_CTRL_ON);
-		tkl_AltNotEsc.consumeKeyDownEvent(event);
+		tkl_AltNotEsc.keyDown(event);
 		byte[] res = tkl_AltNotEsc.getCharSequence();
 		assertNotNull(res);
 		assertEquals(0x03, res[0]);
@@ -57,7 +57,7 @@ public class TermKeyListenerTest extends  AndroidTestCase {
 	
 	public void testKey_Alt_x() {
 			KeyEvent event = new KeyEvent(1,2, KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_X, 0,  KeyEvent.META_ALT_ON);
-			tkl_AltIsEsc.consumeKeyDownEvent(event);
+			tkl_AltIsEsc.keyDown(event);
 			byte[] res = tkl_AltIsEsc.getCharSequence();
 			assertNotNull(res);
 			assertEquals(0x1b, res[0]);
@@ -66,7 +66,7 @@ public class TermKeyListenerTest extends  AndroidTestCase {
 	
 	public void testKey_Alt_x_no_esc() {
 		KeyEvent event = new KeyEvent(1,2, KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_X, 0,  KeyEvent.META_ALT_ON);
-		tkl_AltNotEsc.consumeKeyDownEvent(event);
+		tkl_AltNotEsc.keyDown(event);
 		byte[] res = tkl_AltNotEsc.getCharSequence();
 		assertNull(res);
 	}
@@ -74,7 +74,7 @@ public class TermKeyListenerTest extends  AndroidTestCase {
 
 	public void testKey_Alt_e() {
 			KeyEvent event = new KeyEvent(1,2, KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_E, 0,  KeyEvent.META_ALT_ON);
-			tkl_AltIsEsc.consumeKeyDownEvent(event);
+			tkl_AltIsEsc.keyDown(event);
 			byte res[] = tkl_AltIsEsc.getCharSequence();
 			assertNotNull(res);
 			assertEquals(0x1b, res[0]);
@@ -83,7 +83,7 @@ public class TermKeyListenerTest extends  AndroidTestCase {
 
 	public void testKey_enter() {
 			KeyEvent event = new KeyEvent(1,2, KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER, 0,  0);
-			tkl_AltIsEsc.consumeKeyDownEvent(event);
+			tkl_AltIsEsc.keyDown(event);
 			byte[] res = tkl_AltIsEsc.getCharSequence();
 			assertNotNull(res);
 			assertEquals(0x0d, res[0]);
@@ -91,7 +91,7 @@ public class TermKeyListenerTest extends  AndroidTestCase {
 	
 	public void testKey_del() throws UnsupportedEncodingException {
 		KeyEvent event = new KeyEvent(1,2, KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DEL,0 ,0);
-		tkl_AltIsEsc.consumeKeyDownEvent(event);
+		tkl_AltIsEsc.keyDown(event);
 		byte[] res = tkl_AltIsEsc.getCharSequence();
 		byte[] exp = "\177".getBytes("UTF-8");
 		assertNotNull(res);
