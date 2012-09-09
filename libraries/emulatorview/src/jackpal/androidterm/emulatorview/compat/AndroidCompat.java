a98b5f165b7bc13b188e295adf961c89da7563fe
Jack Palevich <jackpal@google.com>
2012-09-09 11:31:17 -0700

    Harden SDK version detection.
    Use android.os.Build.VERSION.SDK_INT if it is available.
    
    Catch parse errors when parsing at VERSION.SDK.

--- libraries/emulatorview/src/jackpal/androidterm/emulatorview/compat/AndroidCompat.java
+++ libraries/emulatorview/src/jackpal/androidterm/emulatorview/compat/AndroidCompat.java
@@ -14,6 +14,27 @@ package jackpal.androidterm.emulatorview.compat;
  */
 
 public class AndroidCompat {
-    public final static int SDK =
-        Integer.valueOf(android.os.Build.VERSION.SDK);
+    public final static int SDK = getSDK();
+
+    private final static int getSDK() {
+        int result;
+        try {
+            result = AndroidLevel4PlusCompat.getSDKInt();
+        } catch (VerifyError e) {
+            // We must be at an SDK level less than 4.
+            try {
+                result = Integer.valueOf(android.os.Build.VERSION.SDK);
+            } catch (NumberFormatException e2) {
+                // Couldn't parse string, assume the worst.
+                result = 1;
+            }
+        }
+        return result;
+    }
+}
+
+class AndroidLevel4PlusCompat {
+    static int getSDKInt() {
+        return android.os.Build.VERSION.SDK_INT;
+    }
 }
