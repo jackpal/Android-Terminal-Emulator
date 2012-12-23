29b463f4bc3839285999131147850eb33f5722e1
Jack Palevich <jackpal@google.com>
2012-12-23 11:53:09 -0800

    Add some larger font point sizes.
    Fix bug that capped stored font size preference at 20.

--- src/jackpal/androidterm/util/TermSettings.java
+++ src/jackpal/androidterm/util/TermSettings.java
@@ -165,7 +165,7 @@ public class TermSettings {
         mActionBarMode = readIntPref(ACTIONBAR_KEY, mActionBarMode, ACTION_BAR_MODE_MAX);
         // mCursorStyle = readIntPref(CURSORSTYLE_KEY, mCursorStyle, 2);
         // mCursorBlink = readIntPref(CURSORBLINK_KEY, mCursorBlink, 1);
-        mFontSize = readIntPref(FONTSIZE_KEY, mFontSize, 20);
+        mFontSize = readIntPref(FONTSIZE_KEY, mFontSize, 288);
         mColorId = readIntPref(COLOR_KEY, mColorId, COLOR_SCHEMES.length - 1);
         mUTF8ByDefault = readBooleanPref(UTF8_KEY, mUTF8ByDefault);
         mBackKeyAction = readIntPref(BACKACTION_KEY, mBackKeyAction, BACK_KEY_MAX);
