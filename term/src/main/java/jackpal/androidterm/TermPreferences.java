c3522cbb0eb085f19bde7fc55ef5a97627dc058e
dvhh <dinh@ifrec.osaka-u.ac.jp>
2015-04-08 19:30:07 +0900

    File picker for Custom font file, replacing the textfield type preference.
    Implementation assistance :
    
    - [Get intent result from preference screen](http://stackoverflow.com/a/3385285/105104)
    - [Get a file picker intent and get the returned path](http://stackoverflow.com/a/7857102/105104)

--- term/src/main/java/jackpal/androidterm/TermPreferences.java
+++ term/src/main/java/jackpal/androidterm/TermPreferences.java
@@ -19,19 +19,42 @@ package jackpal.androidterm;
 import jackpal.androidterm.compat.ActionBarCompat;
 import jackpal.androidterm.compat.ActivityCompat;
 import jackpal.androidterm.compat.AndroidCompat;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceCategory;
+import android.preference.PreferenceManager;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class TermPreferences extends PreferenceActivity {
     private static final String ACTIONBAR_KEY = "actionbar";
     private static final String CATEGORY_SCREEN_KEY = "screen";
 
+    private static final String CUSTOM_FONT_CHOOSER="custom_font_filepath";
+
+    private static final int RESULT_CODE_FONT_CHOOSER=1;
+
+
+    protected void updateFontSummary() {
+        Preference fontSelector=findPreference(CUSTOM_FONT_CHOOSER);
+        String currentFont= PreferenceManager.getDefaultSharedPreferences(this).getString(CUSTOM_FONT_CHOOSER, "");
+        if(currentFont=="") {
+            fontSelector.setSummary(this.getString(R.string.custom_font_filepath_summary));
+        }else{
+            fontSelector.setSummary(currentFont+" "+this.getString(R.string.custom_font_filepath_summary_reset));
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
@@ -53,7 +76,33 @@ public class TermPreferences extends PreferenceActivity {
                 bar.setDisplayOptions(ActionBarCompat.DISPLAY_HOME_AS_UP, ActionBarCompat.DISPLAY_HOME_AS_UP);
             }
         }
+
+        /// Init custom font chooser
+        Preference fontSelector=findPreference(CUSTOM_FONT_CHOOSER);
+        updateFontSummary();
+        final PreferenceActivity that=this;
+        fontSelector.setOnPreferenceClickListener(
+                new Preference.OnPreferenceClickListener() {
+                    @Override
+                    public boolean onPreferenceClick(Preference preference) {
+                        String currentFont= PreferenceManager.getDefaultSharedPreferences(that).getString(CUSTOM_FONT_CHOOSER, "");
+                        if(currentFont=="") {
+                            Intent intent = new Intent("android.intent.action.GET_CONTENT");
+                            intent.setType("*/*");
+                            startActivityForResult(intent, RESULT_CODE_FONT_CHOOSER);
+                        }else{
+                            SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(that).edit();
+                            editor.putString(CUSTOM_FONT_CHOOSER, "");
+                            editor.commit();
+                            updateFontSummary();
+                        }
+                        return true;
+                    }
+                }
+        );
     }
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -65,5 +114,25 @@ public class TermPreferences extends PreferenceActivity {
         default:
             return super.onOptionsItemSelected(item);
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if(resultCode==RESULT_OK) {
+            if(requestCode==RESULT_CODE_FONT_CHOOSER) {
+                String path= data.getData().getPath();
+                if(new File(path).exists()) {
+                    SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(this).edit();
+                    editor.putString(CUSTOM_FONT_CHOOSER, path);
+                    editor.commit();
+                    updateFontSummary();
+                }else{
+                    Toast.makeText(this,R.string.custom_font_filepath_summary_error,Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
