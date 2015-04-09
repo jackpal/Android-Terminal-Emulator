802caa78e4c63565320ed2cbcd89136ce2a35d72
dvhh <dinh@ifrec.osaka-u.ac.jp>
2015-04-09 17:25:15 +0900

    More checks in font selection
    Hide the preference in API < 4
    Check for file presence & extension
    
    Would still fail if the file is moved or maliciously replaced

--- term/src/main/java/jackpal/androidterm/TermPreferences.java
+++ term/src/main/java/jackpal/androidterm/TermPreferences.java
@@ -19,7 +19,9 @@ package jackpal.androidterm;
 import jackpal.androidterm.compat.ActionBarCompat;
 import jackpal.androidterm.compat.ActivityCompat;
 import jackpal.androidterm.compat.AndroidCompat;
+import jackpal.androidterm.compat.TypefaceCompat;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -27,6 +29,7 @@ import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Toast;
 
@@ -36,19 +39,40 @@ public class TermPreferences extends PreferenceActivity {
     private static final String ACTIONBAR_KEY = "actionbar";
     private static final String CATEGORY_SCREEN_KEY = "screen";
 
-    private static final String CUSTOM_FONT_CHOOSER="custom_font_filepath";
+    private static final String CUSTOM_FONT_CHOOSER_KEY ="custom_font_filepath";
+    private static final String CATEGORY_TEXT_KEY = "text_category";
 
     private static final int RESULT_CODE_FONT_CHOOSER=1;
 
 
     protected void updateFontSummary() {
-        Preference fontSelector=findPreference(CUSTOM_FONT_CHOOSER);
-        String currentFont= PreferenceManager.getDefaultSharedPreferences(this).getString(CUSTOM_FONT_CHOOSER, "");
+        Preference fontSelector=findPreference(CUSTOM_FONT_CHOOSER_KEY);
+        String currentFont= PreferenceManager.getDefaultSharedPreferences(this).getString(CUSTOM_FONT_CHOOSER_KEY, "");
         if(currentFont=="") {
-            fontSelector.setSummary(this.getString(R.string.custom_font_filepath_summary));
+            fontSelector.setSummary(getString(R.string.custom_font_filepath_summary));
         }else{
-            fontSelector.setSummary(currentFont+" "+this.getString(R.string.custom_font_filepath_summary_reset));
+            fontSelector.setSummary(currentFont + " " + getString(R.string.custom_font_filepath_summary_reset));
         }
+    }
+
+    protected boolean tryIntent(String intentDescription) {
+        boolean result;
+        try {
+            Intent intent = new Intent(intentDescription);
+            intent.setType("*/*");
+            /*
+            File path = new File(Environment.getRootDirectory(), "fonts");
+            Log.d(TermPreferences.class.getName(),path.getAbsolutePath());
+            intent.setData(Uri.fromFile(path));
+            */
+            startActivityForResult(intent, RESULT_CODE_FONT_CHOOSER);
+
+            result=true;
+        }catch(ActivityNotFoundException ex) {
+            Log.e(TermPreferences.class.getName(),intentDescription,ex);
+            result=false;
+        }
+        return result;
     }
 
     @Override
@@ -62,11 +86,11 @@ public class TermPreferences extends PreferenceActivity {
         // Remove the action bar pref on older platforms without an action bar
         if (AndroidCompat.SDK < 11) {
             Preference actionBarPref = findPreference(ACTIONBAR_KEY);
-             PreferenceCategory screenCategory =
-                    (PreferenceCategory) findPreference(CATEGORY_SCREEN_KEY);
-             if ((actionBarPref != null) && (screenCategory != null)) {
-                 screenCategory.removePreference(actionBarPref);
-             }
+            PreferenceCategory screenCategory =
+                   (PreferenceCategory) findPreference(CATEGORY_SCREEN_KEY);
+            if ((actionBarPref != null) && (screenCategory != null)) {
+                screenCategory.removePreference(actionBarPref);
+            }
         }
 
         // Display up indicator on action bar home button
@@ -77,29 +101,48 @@ public class TermPreferences extends PreferenceActivity {
             }
         }
 
+
         /// Init custom font chooser
-        Preference fontSelector=findPreference(CUSTOM_FONT_CHOOSER);
-        updateFontSummary();
-        final PreferenceActivity that=this;
-        fontSelector.setOnPreferenceClickListener(
-                new Preference.OnPreferenceClickListener() {
-                    @Override
-                    public boolean onPreferenceClick(Preference preference) {
-                        String currentFont= PreferenceManager.getDefaultSharedPreferences(that).getString(CUSTOM_FONT_CHOOSER, "");
-                        if(currentFont=="") {
-                            Intent intent = new Intent("android.intent.action.GET_CONTENT");
-                            intent.setType("*/*");
-                            startActivityForResult(intent, RESULT_CODE_FONT_CHOOSER);
-                        }else{
-                            SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(that).edit();
-                            editor.putString(CUSTOM_FONT_CHOOSER, "");
-                            editor.commit();
-                            updateFontSummary();
-                        }
-                        return true;
-                    }
+        Preference fontSelector=findPreference(CUSTOM_FONT_CHOOSER_KEY);
+        if(fontSelector!=null) {
+            if (AndroidCompat.SDK < 4) {
+                PreferenceCategory textCategory = (PreferenceCategory) findPreference(CATEGORY_TEXT_KEY);
+                if(textCategory!=null) {
+                    textCategory.removePreference(fontSelector);
+                }else{
+                    Log.e(TermPreferences.class.getName(), "cannot find 'Text' preference category");
                 }
-        );
+            } else {
+                updateFontSummary();
+                final PreferenceActivity that = this;
+                fontSelector.setOnPreferenceClickListener(
+                        new Preference.OnPreferenceClickListener() {
+                            @Override
+                            public boolean onPreferenceClick(Preference preference) {
+                                String currentFont = PreferenceManager.getDefaultSharedPreferences(that).getString(CUSTOM_FONT_CHOOSER_KEY, "");
+                                if (currentFont == "") {
+                                    if(
+                                            !(tryIntent("android.intent.action.GET_CONTENT") || tryIntent("android.intent.action.PICK"))
+                                            ) {
+                                        // but it really should not happen at all !
+
+                                        Toast.makeText(that,R.string.custom_font_filepath_summary_error_filepicker_Intent,Toast.LENGTH_SHORT).show();
+                                        return false;
+                                    }
+                                } else {
+                                    SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(that).edit();
+                                    editor.putString(CUSTOM_FONT_CHOOSER_KEY, "");
+                                    editor.commit();
+                                    updateFontSummary();
+                                }
+                                return true;
+                            }
+                        }
+                );
+            }
+        }else{
+            Log.e(TermPreferences.class.getName(), "cannot find 'font selector' preference");
+        }
     }
 
 
@@ -116,19 +159,40 @@ public class TermPreferences extends PreferenceActivity {
         }
     }
 
+
+    protected boolean checkFile(String input) {
+        File path=new File(input);
+        boolean result=false;
+        if(path.exists() && path.isFile() &&
+                (path.getName().endsWith(".ttf") || path.getName().endsWith(".otf"))
+                ) {
+            if(TypefaceCompat.createFromFile(path,null)!=null) {
+                result=true;
+            }else{
+                Log.e(TermPreferences.class.getName(),"can not read the font file");
+            }
+        }else{
+            Log.e(TermPreferences.class.getName(),"invalid patgh");
+        }
+        return result;
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
         if(resultCode==RESULT_OK) {
             if(requestCode==RESULT_CODE_FONT_CHOOSER) {
+
                 String path= data.getData().getPath();
-                if(new File(path).exists()) {
+
+
+                if(checkFile(path)) {
                     SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(this).edit();
-                    editor.putString(CUSTOM_FONT_CHOOSER, path);
+                    editor.putString(CUSTOM_FONT_CHOOSER_KEY, path);
                     editor.commit();
                     updateFontSummary();
                 }else{
-                    Toast.makeText(this,R.string.custom_font_filepath_summary_error,Toast.LENGTH_LONG).show();
+                    Toast.makeText(this,R.string.custom_font_filepath_summary_error_filepicker,Toast.LENGTH_LONG).show();
                 }
             }
         }
--- term/src/main/java/jackpal/androidterm/compat/TypefaceCompat.java
+++ term/src/main/java/jackpal/androidterm/compat/TypefaceCompat.java
@@ -14,18 +14,22 @@ import jackpal.androidterm.emulatorview.compat.*;
 public class TypefaceCompat {
     private static class Api4OrLater {
         @TargetApi(4)
-        public static Typeface createFromFile(File path) {
+        public static Typeface createFromFile(File path,Typeface defaultValue) {
             Typeface result=Typeface.createFromFile(path);
             if(result.equals(Typeface.DEFAULT)) {
-                result=Typeface.MONOSPACE;
+                result=defaultValue;
             }
             return result;
         }
     }
     public static Typeface createFromFile(File path) {
+        return createFromFile(path,Typeface.MONOSPACE);
+    }
+
+    public static Typeface createFromFile(File path, Typeface defaulValue) {
         if(jackpal.androidterm.emulatorview.compat.AndroidCompat.SDK < 4 ) {
-            return Typeface.MONOSPACE;
+            return defaulValue;
         }
-        return Api4OrLater.createFromFile(path);
+        return Api4OrLater.createFromFile(path,defaulValue);
     }
 }
